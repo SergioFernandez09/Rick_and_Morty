b42dc506c86dfafee5b6c00010ac37d88e19ef17
SergioFernandez09 <sergiofernandezn2001@gmail.com>
2023-04-03 23:36:59 +0100

    first commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import Cards from "./Cards";
+
+export default function SearchBar({onSearch}) {
+   
+   
+   return (
+      <div>
+         <input type='search' />
+         <button onClick={onSearch}>Agregar</button>
+      </div>
+   );
+}
