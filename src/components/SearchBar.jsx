c1532063485b8f6f0ba84c97b6d8196312ab130f
SergioFernandez09 <sergiofernandezn2001@gmail.com>
2023-04-04 15:14:28 +0100

    Api_Conection_y_OnClose

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import Cards from "./Cards";
 
 export default function SearchBar({onSearch}) {
+   const [id, setId] = useState('');
    
-   
+   const handleChange = (event) => {
+      setId(event.target.value)
+   }
+
    return (
       <div>
-         <input type='search' />
-         <button onClick={onSearch}>Agregar</button>
+         <input type='search' onChange={handleChange} value={id}/>
+         <button onClick={() => onSearch(id)}>Agregar</button>
       </div>
    );
 }
